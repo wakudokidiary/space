696728446ebab5d02f356f412eabb9e0f7171f31
wakudokidiary <77178772+wakudokidiary@users.noreply.github.com>
2021-03-28 23:04:33 +0900

    Add files via upload

--- space.js
+++ space.js
@@ -16,6 +16,32 @@ $('#racket').click(function(){
   $('#ball').fadeIn('slow');
 });
 
+$('#uku_one').click(function(){
+  $(this).fadeOut('slow');
+});
+
+$('#uku_hand').click(function(){
+  $(this).fadeOut('slow');
+});
+
+$('#uku_three').click(function(){
+  $(this).fadeOut('slow');
+});
+
+
+$('#uku_one').click(function(){
+  $('#ukuone').fadeOut('slow');
+});
+
+$('#uku_hand').click(function(){
+  $('#ukutwo').fadeOut('slow');
+});
+
+$('#uku_three').click(function(){
+  $('#ukuthree').fadeOut('slow');
+});
+
+
 
 
 
