c2eb8a19e808ce6e37f8eaa71a9c13ecddd7fc1d
wakudokidiary <77178772+wakudokidiary@users.noreply.github.com>
2021-03-31 21:47:49 +0900

    Add files via upload

--- space.js
+++ space.js
@@ -41,7 +41,26 @@ $('#uku_three').click(function(){
   $('#ukuthree').fadeOut('slow');
 });
 
+  // ここからメニュー
+  $('#bar').click(function(){
+    $('#menu').slideDown();
+  });
 
+  $('#bar').click(function(){
+    $(this).fadeOut();
+  });
+
+  $('#batsu').click(function(){
+    $('#menu').slideUp();
+  });
+
+
+
+  $('#batsu').click(function(){
+    $('#bar').fadeIn();
+  });
+
+// ここまでメニュー
 
 
 
